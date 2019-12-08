a8c06d689094e2c9f2d868b1a0b19f991a805246
Olzzon <github@olzzon.dk>
2019-12-08 20:58:18 +0100

    setup basic send receive

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import { osc } from 'osc'
+import osc from 'osc'
+
+const OSC_SERVER_IP: string = '127.0.0.1'
+const OSC_SERVER_PORT: number = 5255
+
+this.oscConnection = new osc.UDPPort({
+    remoteAddress: OSC_SERVER_IP,
+    remotePort: OSC_SERVER_PORT
+});
+
+this.oscConnection
+.on("ready", () => {
+})
+.on('message', (message: any) => {
+    console.log('Received Message :', message)
+})
+.on('error', (error: any) => {
+})
+
+this.oscConnection.open()
+
+this.oscConnection.send({
+    address: '/state/full',
+})
 
 console.log('RUNNING')
